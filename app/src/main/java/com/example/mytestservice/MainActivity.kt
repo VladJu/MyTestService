54f7c6ba478fa5666c4db6a870945a15ae5500ac
VladJu <juikovvlad@gmail.com>
2023-03-17 23:18:06 +0300

    onStartCommand() return:START_STICY,START_NOT_STICY,START_REDELIVER_INTENT

--- app/src/main/java/com/example/mytestservice/MainActivity.kt
+++ app/src/main/java/com/example/mytestservice/MainActivity.kt
@@ -9,11 +9,12 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.simpleService.setOnClickListener{
-            startService(MyService.newIntent(this))
+        binding.simpleService.setOnClickListener {
+            startService(MyService.newIntent(this, 10))
         }
     }
 }
